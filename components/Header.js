29d3a54f5cfd984ddc44a9c6e7990833e6fba6b8
Shimon-Levinshtein <shimonwebdeveloper@gmail.com>
2020-11-21 19:40:36 +0200

    first-git

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const Header = props => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Header;
